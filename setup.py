c97c4e7044c0ccf3967ac34d16e8cccdb77aa18a
MathieuCayssol <49449000+MathieuCayssol@users.noreply.github.com>
2022-02-04 17:14:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ setuptools.setup(
     author_email='mathieu.cayssol@gmail.com',
     description='Dimension reduction (UMAP) + Density Based clustering (DBSCAN)',
     long_description_content_type="text/markdown",
-    url='https://github.com/MathieuCayssol/DimReduction-Clustering',
+    url='https://github.com/MathieuCayssol/DimReductionClustering',
     project_urls = {
-        "Bug Tracker": "https://github.com/MathieuCayssol/DimReduction-Clustering/issues"
+        "Bug Tracker": "https://github.com/MathieuCayssol/DimReductionClustering/issues"
     },
     license='MIT',
     packages=['toolbox'],
